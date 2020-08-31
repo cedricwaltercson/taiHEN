ec1cd9c6329bb31f0c0c6ea53b526f60d869c8b8
cedricwaltercson <cedricwaltercson@hotmail.com>
2020-08-31 10:04:52 +1000

    Removed deprecated functions and warnings. Added simple build script.

--- plugin.c
+++ plugin.c
@@ -5,7 +5,8 @@
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
  */
-#include <psp2kern/io/fcntl.h>
+//#include <psp2kern/io/fcntl.h>
+#include <psp2kern/kernel/iofilemgr.h>
 #include <psp2kern/kernel/sysmem.h>
 #include <psp2kern/kernel/modulemgr.h>
 #include <psp2kern/kernel/threadmgr.h>
